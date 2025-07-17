d8aa132978872715e25dbd398f0176e70c3d036e
adhli-business <putraadhli.business@gmail.com>
2025-07-17 16:28:37 +0700

    test workflow 1

---
+++ app.js
@@ -0,0 +1,17 @@
+// Fungsi penjumlahan
+function tambah(a, b) {
+return a + b;
+}
+
+// Fungsi pengurangan
+function kurang(a, b) {
+return a - b;
+}
+
+// Ekspor fungsi untuk digunakan di file lain
+module.exports = {
+tambah,
+kurang
+};
+
+
